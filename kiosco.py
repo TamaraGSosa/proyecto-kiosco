876c2f9da61e54d7f277cc0d9c0acf74ca284bce
Facundo Diaz <Fakudiaz389@gmail.com>
2026-04-24 14:44:59 -0300

    Agrego clase Producto en kiosco.py y actualizo README

---
+++ kiosco.py
@@ -0,0 +1,38 @@
+class Producto:
+    def __init__(self,name,price,stock):
+        if price<0:
+            raise ValueError ("El precio no puede ser negativo")
+        if stock <0:
+            raise ValueError ("El stock no puede ser negativo")
+        self.name=name
+        self.price=price
+        self.stock=stock
+
+    def set_price(self, new_price):
+        if new_price <0:
+            raise ValueError ("El precio no puede ser negativo")
+        self.price=new_price
+
+    def  set_stock(self,new_stock):
+        if new_stock <0:
+            raise ValueError ("El stock no puede ser negativo")
+        self.stock=new_stock
+
+# diccionario por si implementamos json        
+#    def to_dict(self):
+#        return{
+#            "name":self.name,
+#            "price":self.price,
+#            "stock":self.stock
+#        }
+
+    def __str__(self):
+        return f"Nombre:{self.name}-Precio:{self.price} ({self.stock} existe en stock)"
+
+cartuchera=Producto("cartuchera",50000,20)
+carpeta=Producto("carpeta",70000,10)
+
+inventario=[cartuchera,carpeta]
+
+for i in inventario:
+    print(i)
